0a40638d1f240fc6fb5bb03c4ab0196448910ac6
Khalid <mustkre8@gmail.com>
2017-01-26 08:35:24 -0800

    Initial Commit

---
+++ RNCompanion/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  RNCompanion
+//
+//  Created by Khalid Mills on 1/26/17.
+//  Copyright © 2017 iksnae. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
---
+++ RNCompanion/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RNCompanion
+//
+//  Created by Khalid Mills on 1/26/17.
+//  Copyright © 2017 iksnae. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
