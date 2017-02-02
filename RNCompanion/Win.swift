c468b33b1719e66ef14fb139671b3adb8d9dd938
Khalid <mustkre8@gmail.com>
2017-02-02 09:58:36 -0800

    updated defaul frame and position

--- RNCompanion/Win.swift
+++ RNCompanion/Win.swift
@@ -13,6 +13,12 @@ class Win: NSWindowController {
     override func windowDidLoad() {
         super.windowDidLoad()
         window?.level = Int(CGWindowLevelForKey(.floatingWindow))
+        if let window = window {
+            let screenRect = (NSScreen.main()?.visibleFrame)!
+            window.level = Int(CGWindowLevelForKey(.floatingWindow))
+            window.setFrame(CGRect(x: screenRect.width-100, y: screenRect.height-20, width: 200, height: 120), display: true)
+            
+        }
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
 
